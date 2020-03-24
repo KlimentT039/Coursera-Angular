64e753b34cc236f9a8a42d28a193031cd6a67f4f
KlimentT039 <49238037+KlimentT039@users.noreply.github.com>
2020-03-24 20:53:15 +0100

    Add files via upload

---
+++ Lecture08/app.js
@@ -0,0 +1,20 @@
+(function() {
+'use strict'
+
+angular.module('DIApp',[])
+
+.controller('DIController',DIController);
+
+DIController.$inject=["$scope","$filter"];
+
+function DIController($scope, $filter, $injector){
+  $scope.name="Kliment";
+
+  $scope.upper= function () {
+    var upCase = $filter('uppercase');
+    $scope.name = upCase($scope.name);
+  };
+
+}
+
+})();
